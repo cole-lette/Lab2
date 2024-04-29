e272976f01e80f75023df4ef567905d2130b20b5
Nicole <nicolechuajt21.23@ichat.sp.edu.sg>
2024-04-29 16:43:40 +0800

    different return values of different functions

---
+++ Exercise2.py
@@ -0,0 +1,12 @@
+def display_main_menu():
+    print("display_main_menu")
+def calc_average():
+    print("calc_average")
+def get_user_input():
+    print("get_user_input")
+def find_min_max():
+    print("find_min_max")
+def sort_temperature():
+    print("sort_temperature")
+def calc_median_temperature():
+    print("calc_median_temperature")
